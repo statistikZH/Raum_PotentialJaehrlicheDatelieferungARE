5a9c3aefaac90194b4ea316d1800ce4648541020
Katharina Kaelin <katharina.kaelin@statistik.ji.zh.ch>
2023-08-04 08:53:49 +0000

    Aktualisierung 2023

--- PotentialJaehrlicheDatelieferungARE.R
+++ PotentialJaehrlicheDatelieferungARE.R
@@ -19,11 +19,12 @@ options(scipen = 1000000)
 options(digits = 6)
 
 # definiere Stichtage ----
-gde_stichtag = '31.12.2021'
-geb_stichtag = '31.12.2021'
-einw_stichtag = '31.12.2021'
-besch_stichtag = '31.12.2019'
-gfb_jahr = '2021'
+gde_stichtag = '01.01.2023'
+geb_stichtag = '31.12.2022'
+einw_stichtag = '31.12.2022'
+besch_stichtag = '31.12.2020'
+oereb_stichtag = "2023_01_18"
+gfb_jahr = '2022'
 
 # import ----
 
@@ -32,6 +33,12 @@ gde <- tbl(con, "GEMEINDE") %>%
   # lese nur Reihen mit STICHTAG = Stichtag ein.
   filter(to_date(!!gde_stichtag,'dd.mm.yyyy') >= VON & to_date(!!gde_stichtag,'dd.mm.yyyy') <= BIS) %>%
   select(GDE_BFSNR, HANDLRAUM_NAME) %>%
+  collect() %>%
+  arrange(GDE_BFSNR)
+
+
+## gemeindefus ----
+gemeindefus <- tbl(con, "GEMEINDEFUS") %>%
   collect()
 
 ## geb ----
@@ -59,10 +66,9 @@ einw <- tbl(con, in_schema("EINWOHNER","EINW_BASIS")) %>%
   collect() %>%
   {. ->>  einw_temp1} %>%
   left_join(geb, by = c("DW_ADDR_EGID" = "EGID")) %>%
-  left_join(gde, by = c("CFILE" = "GDE_BFSNR")) %>%
   {. ->>  einw_temp2} %>%
   filter(!is.na(GKODE) | !is.na(GKODN)) %>%
-  group_by(GKODE, GKODN, HANDLRAUM_NAME, CFILE) %>%
+  group_by(GKODE, GKODN) %>%
   summarize(einw_anz = n()) %>%
   ungroup() %>%
   st_as_sf(coords=c("GKODE", "GKODN"), crs = 2056)
@@ -81,33 +87,57 @@ besch <- tbl(con, in_schema("OEC", "STATENT_LOC_MV")) %>%
   filter(STICHTAG == to_date(besch_stichtag,'dd.mm.yyyy')) %>%
   # lese nur Reihen im Kanton Zürich ein.
   filter(CANTON_CD == '1') %>%
+  filter(!is.na(GKODE) | !is.na(GKODN)) %>%
   select(EMPTOT, MUNICIPALITY_CD, GKODE, GKODN) %>%
   collect() %>%
   {. ->>  besch_temp1} %>%
-  left_join(gde, by = c("MUNICIPALITY_CD" = "GDE_BFSNR")) %>%
-  filter(!is.na(GKODE) | !is.na(GKODN)) %>%
-  group_by(GKODE, GKODN, HANDLRAUM_NAME,MUNICIPALITY_CD) %>%
+  group_by(GKODE, GKODN) %>%
   summarize(besch_anz = sum(EMPTOT)) %>%
   ungroup() %>%
   st_as_sf(coords=c("GKODE", "GKODN"), crs = 2056)
 
-# Anteil besch mit geok
-sum(besch$besch_anz)/nrow(besch_temp1)*100
-# should be == TRUE
-sum(besch$besch_anz) == nrow(besch_temp1)
+# check
+## Anteil besch mit geok
+sum(besch$besch_anz) == sum(besch_temp1$EMPTOT)
+
+rm(besch_temp1)
 
 ## oereb ----
-oereb <- st_read("~/file-server/file-server/08_DS/03_GIS/Geodaten/NP_GN_ZONENFLAECHE_F_KTZH/NP_GN_ZONENFLAECHE_F_KTZH_2022_03_10.shp", stringsAsFactors = FALSE, crs=2056) %>%
-  select(TYP_ZH_COD, TYP_BFSNR)
+oereb <- st_read(paste0("~/file-server/file-server/08_DS/03_GIS/Geodaten/NP_GN_ZONENFLAECHE_F_KTZH/NP_GN_ZONENFLAECHE_F_KTZH_", oereb_stichtag, ".shp"), stringsAsFactors = FALSE, crs=2056) %>%
+  select(TYP_ZH_COD, TYP_BFSNR) %>%
+  {. ->>  oereb_temp1} %>%
+  left_join(gde, by = c("TYP_BFSNR" = "GDE_BFSNR"))
+
+# check
+## should be == TRUE
+nrow(oereb) == nrow(oereb_temp1)
+oereb_check_hr <- oereb %>%
+  st_drop_geometry() %>%
+  distinct(HANDLRAUM_NAME, TYP_BFSNR)
+length(oereb_check_hr) == length(gde)
 
 ## gfb ----
-
 gfb_path <- paste0("~/file-server/file-server/03_AS/02_Datengrundlagen/GFL/PotentialJaehrlicheDatelieferungARE/", gfb_jahr, "/")
 gfb <- data.table::fread(paste0(gfb_path, "Input/GFB_InputPotential_", gfb_jahr, ".csv")) %>%
-  select(TYP_GDE_CODE, GFB, BFSNR) %>%
+  rename(BFSNR = 1, TYP_GDE_CODE = 2, XXX = 3, GFB = 4) %>%
+  select(BFSNR, TYP_GDE_CODE, GFB) %>%
   mutate(TYP_ZH_COD = substr(TYP_GDE_CODE, start = 1, stop = 5)) %>%
   select(-TYP_GDE_CODE) %>%
+  {. ->>  gfb_temp1} %>%
+  left_join(gemeindefus, by=c('BFSNR' = 'GEMEINDEFUS_ALT'), suffix = c("", ".y")) %>%
+  {. ->>  gfb_temp2} %>%
+  select_at(vars(-ends_with(".y"))) %>%
+  # korrigiere  Spalte 'BFSNR' für Fusionen
+  mutate(BFSNR = case_when(
+    !is.na(GEMEINDEFUS_NEU) ~ GEMEINDEFUS_NEU,
+    TRUE ~ BFSNR)) %>%
   left_join(gde, by = c("BFSNR" = "GDE_BFSNR"))
+
+#check
+## should be == TRUE
+nrow(gfb) == nrow(gfb_temp1)
+# should NOT be TRUE # xxx
+table(is.na(gfb$HANDLRAUM_NAME) , useNA = c("always"))
 
 # aggregate by TYP_ZH_COD ----
 
@@ -115,7 +145,7 @@ gfb <- data.table::fread(paste0(gfb_path, "Input/GFB_InputPotential_", gfb_jahr,
 einw_oereb <- einw %>%
   st_join(oereb) %>%
   st_drop_geometry() %>%
-  group_by(TYP_ZH_COD, HANDLRAUM_NAME, CFILE) %>%
+  group_by(TYP_ZH_COD, HANDLRAUM_NAME, TYP_BFSNR) %>%
   summarize(einw_anz = sum(einw_anz, na.rm = TRUE)) %>%
   ungroup()
 
@@ -128,7 +158,7 @@ rm(einw)
 besch_oereb <- besch %>%
   st_join(oereb) %>%
   st_drop_geometry() %>%
-  group_by(TYP_ZH_COD, HANDLRAUM_NAME,MUNICIPALITY_CD) %>%
+  group_by(TYP_ZH_COD, HANDLRAUM_NAME, TYP_BFSNR) %>%
   summarize(besch_anz = sum(besch_anz, na.rm = TRUE)) %>%
   ungroup()
 
@@ -152,8 +182,11 @@ rm(gfb)
 ## prep_oereb ----
 prep_oereb <- oereb %>%
   st_drop_geometry() %>%
-  left_join(gde, by = c("TYP_BFSNR" = "GDE_BFSNR")) %>%
   distinct(TYP_ZH_COD, HANDLRAUM_NAME, TYP_BFSNR)
+
+# check
+prop.table(table(prep_oereb$HANDLRAUM_NAME, useNA = c("always")))*100
+table(is.na(prep_oereb$HANDLRAUM_NAME) , useNA = c("always"))
 
 ## gfb_oereb_antijoin ----
 # Nicht alle Attributkombinationen von gfb::'TYP_ZH_COD', 'BFSNR', 'HANDLRAUM_NAME' (gfb_distinct; siehe unten) kommen im oereb (prep_oereb) vor
@@ -161,17 +194,21 @@ prep_oereb <- oereb %>%
 gfb_distinct <- gfb_oereb %>% select(-gfb_m2) %>% distinct()
 ## Attributkombinationen die im oereb nicht vorkommen
 gfb_oereb_antijoin <- anti_join(gfb_distinct, prep_oereb, by = c("TYP_ZH_COD", "HANDLRAUM_NAME", "BFSNR" = "TYP_BFSNR")) %>%
-  rename(TYP_BFSNR = BFSNR)
-#oereb_gfb_antijoin <- anti_join(prep_oereb, gfb_distinct, by = c("TYP_ZH_COD", "HANDLRAUM_NAME", "TYP_BFSNR" = "BFSNR"))
+  rename(TYP_BFSNR = BFSNR) %>%
+  filter()
 
 # output final: TYP_ZH_COD_df ----
 # join df to prep_oereb
 TYP_ZH_COD_df <- prep_oereb %>%
   bind_rows(gfb_oereb_antijoin) %>%
+  {. ->>  TYP_ZH_COD_temp1} %>%
   #add_row(TYP_ZH_COD = "C1304", HANDLRAUM_NAME = "Landschaft unter Druck", TYP_BFSNR = 83) %>% # fehlt, check sum(gfb_m2) unten stimmt sonst nicht
-  left_join(einw_oereb, by = c("TYP_ZH_COD", "HANDLRAUM_NAME","TYP_BFSNR" = "CFILE")) %>%
-  left_join(besch_oereb, by = c("TYP_ZH_COD", "HANDLRAUM_NAME", "TYP_BFSNR" = "MUNICIPALITY_CD")) %>%
+  left_join(einw_oereb, by = c("TYP_ZH_COD", "HANDLRAUM_NAME","TYP_BFSNR")) %>%
+  {. ->>  TYP_ZH_COD_temp2} %>%
+  left_join(besch_oereb, by = c("TYP_ZH_COD", "HANDLRAUM_NAME", "TYP_BFSNR")) %>%
+  {. ->>  TYP_ZH_COD_temp3} %>%
   left_join(gfb_oereb, by = c("TYP_ZH_COD", "HANDLRAUM_NAME", "TYP_BFSNR" = "BFSNR")) %>%
+  {. ->>  TYP_ZH_COD_temp4} %>%
   mutate(TYP_ZH_COD_aggr = case_when(
     TYP_ZH_COD %in% c("C1101", "C1102", "C1103", "C1104", "C1105") ~ "Wohnzone",
     TYP_ZH_COD %in% c("C1201", "C1202", "C1203") ~ "Arbeitszone",
@@ -204,6 +241,7 @@ TYP_ZH_COD_df <- prep_oereb %>%
 
 # check
 ## should be == TRUE
+nrow(TYP_ZH_COD_temp1) == nrow(TYP_ZH_COD_temp4)
 sum(TYP_ZH_COD_df_temp$einw_anz) == sum(einw_oereb$einw_anz)
 sum(TYP_ZH_COD_df_temp$besch_anz) == sum(besch_oereb$besch_anz)
 sum(TYP_ZH_COD_df_temp$gfb_m2) == sum(gfb_oereb$gfb_m2)
@@ -211,7 +249,6 @@ sum(TYP_ZH_COD_df_temp$gfb_m2) == sum(gfb_oereb$gfb_m2)
 # export ----
 
 ## xlsx ----
-#file.remove(paste0(output_path, "/Arbeitszonen_Region_", auswertungsjahr, ".xlsx"))
 statR::quickXLSX(data = TYP_ZH_COD_df,
                  title = paste0("GFB_AuswertungPotential_", gfb_jahr),
                  file = paste0(gfb_path, "GFB_AuswertungPotential_", gfb_jahr), # '.xlsx' wird automatisch hinzugefügt
@@ -222,7 +259,7 @@ statR::quickXLSX(data = TYP_ZH_COD_df,
                  grouplines = FALSE,
                  author = "KK")
 
-##  csv----
+##  csv ----
 write.table(TYP_ZH_COD_df, paste0(gfb_path, "GFB_AuswertungPotential_", gfb_jahr, ".csv"), row.names=FALSE, sep = ",", fileEncoding ="UTF-8")
 
 # weiteres ----
